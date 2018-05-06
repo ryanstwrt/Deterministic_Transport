Fixed current error, and created histogram/cell average flux graph.

Driver.py
@@ -16,18 +16,29 @@ def step_characteristic(angular_flux_pos_lhs, angular_flux_pos_rhs, scalar_flux_
             for cell_num, cell in enumerate(material_cell):
                 tau = material_data.ix[total, cell] * cell_width / abs(mu_n[(0, angle)])
 
-                angular_flux_pos_rhs[(angle, nrg)] = angular_flux_pos_lhs[(angle, nrg)] * np.exp(-tau) + (Q[cell_num]
-                                                    / material_data.ix[total, cell]) * (1 - np.exp(-tau))
+                if cell_num == 0:
+                    angular_flux_pos_rhs[(angle, nrg)] = angular_flux_pos_lhs[(9 - angle, nrg)] * np.exp(-tau) \
+                                + (Q[cell_num] / material_data.ix[total, cell]) * (1 - np.exp(-tau))
 
-                cell_average_angular_flux = (cell_width * Q[cell_num] - mu_n[(0, angle)] *
+                    cell_average_angular_flux = (cell_width * Q[cell_num] - mu_n[(0, angle)]
+                                * (angular_flux_pos_rhs[(angle, nrg)] - angular_flux_pos_lhs[(9 - angle, nrg)])) \
+                                / (material_data.ix[total, cell] * cell_width)
+
+                else:
+                    angular_flux_pos_rhs[(angle, nrg)] = angular_flux_pos_lhs[(angle, nrg)] * np.exp(-tau) + (Q[cell_num]
+                                            / material_data.ix[total, cell]) * (1 - np.exp(-tau))
+
+                    cell_average_angular_flux = (cell_width * Q[cell_num] - mu_n[(0, angle)] *
                                             (angular_flux_pos_rhs[(angle, nrg)] - angular_flux_pos_lhs[(angle, nrg)])) \
                                             / (material_data.ix[total, cell] * cell_width)
 
+                cell_edge_flux_new[(cell_num, nrg)] += angular_flux_pos_rhs[(angle, nrg)] * mu_n[(1, angle)]
                 scalar_flux_new[(cell_num, nrg)] += cell_average_angular_flux * mu_n[(1, angle)]
                 current_new[(cell_num, nrg)] += cell_average_angular_flux * mu_n[(1, angle)] * mu_n[(0, angle)]
-                angular_flux_pos_lhs[(angle, nrg)] = angular_flux_pos_rhs[(angle, nrg)]
+                cell_edge_current_new[(cell_num, nrg)] += angular_flux_pos_rhs[(angle, nrg)] * mu_n[(1, angle)] * mu_n[(0, angle)]
+                angular_flux_pos_lhs[:, nrg] = angular_flux_pos_rhs[:, nrg]
 
-            # for mu < 0, we remove the - sign, as the change in delta x would create an additional negative to cancel
+        # for mu < 0, we remove the - sign, as the change in delta x would create an additional negative to cancel
         else:
             for cell_num, cell in enumerate(reversed(material_cell)):
                 rev_cell_num = 127 - cell_num
@@ -53,9 +64,11 @@ def step_characteristic(angular_flux_pos_lhs, angular_flux_pos_rhs, scalar_flux_
                                                     - angular_flux_pos_lhs[(angle, nrg)])) \
                                                 / (material_data.ix[total, cell] * cell_width)
 
+                cell_edge_flux_new[(cell_num, nrg)] += angular_flux_pos_lhs[(angle, nrg)] * mu_n[(1, angle)]
                 scalar_flux_new[(rev_cell_num, nrg)] += cell_average_angular_flux * mu_n[(1, angle)]
                 current_new[(rev_cell_num, nrg)] += cell_average_angular_flux * mu_n[(1, angle)] * mu_n[(0, angle)]
-                angular_flux_pos_rhs[(angle, nrg)] = angular_flux_pos_lhs[(angle, nrg)]
+                cell_edge_current_new[(cell_num, nrg)] += angular_flux_pos_lhs[(angle, nrg)] * mu_n[(1, angle)] * mu_n[(0, angle)]
+                angular_flux_pos_rhs[:, nrg] = angular_flux_pos_lhs[:, nrg]
 
     return
 
@@ -76,7 +89,7 @@ materials_b = [[0.2, 0.185, 0.015, 0.0, 1.0, 1.2, 0.9, 0.0, 0.45, 0.0],
                [0.2, 0.17, 0.03, 0.0, 0.0, 1.1, 1.1, 0.0, 0.0, 0.0]]
 
 # Create a data from for the material cross sections to be used for each material
-material_data = pd.DataFrame(materials_b, columns=['total_fast', 'inscatter_fast', 'downscatter_fast',
+material_data = pd.DataFrame(materials_a, columns=['total_fast', 'inscatter_fast', 'downscatter_fast',
                                                    'nusigmaf_fast', 'chi_fast', 'total_thermal', 'inscatter_thermal',
                                                    'downscatter_thermal', 'nusigmaf_thermal', 'chi_thermal'],
                              index=['MOX', 'U', 'water'])
@@ -92,6 +105,10 @@ k_old = 1
 k_new = 0
 fission_source_old = np.ones(128)
 fission_source_new = np.ones(128)
+cell_edge_flux_new = np.zeros((128, 2))
+cell_edge_current_new = np.zeros((128, 2))
+cell_edge_flux_old = np.zeros((128, 2))
+cell_edge_current_old = np.zeros((128, 2))
 scalar_flux_old = np.zeros((128, 2))
 current_old = np.zeros((128, 2))
 angular_flux_pos_lhs = np.zeros((10, 2))
@@ -114,6 +131,7 @@ num_power_iter = 0
 while k_conv < k_convergence or fission_source_conv < fs_convergence:
     num_source_iter_fast = 0
     num_source_iter_thermal = 0
+
     # loop over the energy groups (we only have two energy groups:
     # 0 for fast and 1 for thermal
     for energy_group in [0, 1]:
@@ -129,6 +147,8 @@ while k_conv < k_convergence or fission_source_conv < fs_convergence:
         while 1 < 2:  # source_convergence < source_convergence_test:
             scalar_flux_new = np.zeros((128, 2))
             current_new = np.zeros((128, 2))
+            cell_edge_current_new = np.zeros((128, 2))
+            cell_edge_flux_new = np.zeros((128, 2))
 
             if energy_group == 0:
                 for cell_num, cell in enumerate(material_cell):
@@ -152,7 +172,8 @@ while k_conv < k_convergence or fission_source_conv < fs_convergence:
                                         / np.amax(scalar_flux_new[:, energy_group]))
             scalar_flux_old[:, energy_group] = scalar_flux_new[:, energy_group]
             current_old[:, energy_group] = current_new[:, energy_group]
-            #print("Source Convergence: %0.8f" % group_source_convergence)
+            cell_edge_flux_old[:, energy_group] = cell_edge_flux_new[:, energy_group]
+            cell_edge_current_old[:, energy_group] = cell_edge_current_new[:, energy_group]
             if group_source_convergence < source_convergence:
                 break
 
@@ -184,20 +205,29 @@ pin_cell_average = np.concatenate(([fast], [thermal]))
 pin_cell_average = pin_cell_average.T
 
 # Write out the flux, current and pin average flux to excel
-source_excel = pd.DataFrame(scalar_flux_old)
+flux_excel = pd.DataFrame(scalar_flux_old)
+edge_flux_excel = pd.DataFrame(cell_edge_flux_old)
 current_excel = pd.DataFrame(current_old)
+edge_current_excel = pd.DataFrame(cell_edge_current_old)
 pin_average_excel = pd.DataFrame(pin_cell_average)
-filepath = 'deterministic.xlsx'
 
-source_excel.to_excel(filepath, index=False, sheet_name='source')
-current_excel.to_excel(filepath, index=False, sheet_name='current')
-pin_average_excel.to_excel(filepath, index=False, sheet_name='pin_average')
+filepath = 'deterministic.xlsx'
+writer = pd.ExcelWriter(filepath)
+
+flux_excel.to_excel(writer, index=False, sheet_name='cell_flux')
+edge_flux_excel.to_excel(writer, index=False, sheet_name='cell_edge_flux')
+current_excel.to_excel(writer, index=False, sheet_name='current')
+edge_current_excel.to_excel(writer, index=False, sheet_name='cell_edge_current')
+pin_average_excel.to_excel(writer, index=False, sheet_name='pin_average')
+writer.save()
 
 # Print out plots of each
+pp.flux_histogram(pin_cell_average, "Pin Averaged Flux with Cell Average Flux", "Pin Cell", "Flux (1/cm^2)", "Fast Flux", "Thermal Flux", scalar_flux_old)
 pp.plot_flux(scalar_flux_old, "Flux", "Cell", "Flux (1/cm^2)", "Fast Flux", "Thermal Flux")
+pp.plot_flux(cell_edge_flux_old, "Cell Edge Flux", "Cell Edge", "Flux (1/cm^2)", "Fast Flux", "Thermal Flux")
 pp.plot_1d_array(fission_source_new, "Fission Source", "Cell", "Unscaled Probability", "Fission Source")
-pp.plot_flux(current_old, "Current", "Cell #", "Flux (1/cm^2)", "Fast Current", "Thermal Current")
+pp.plot_flux(current_old, "Current", "Cell", "Flux (1/cm^2)", "Fast Current", "Thermal Current")
+pp.plot_flux(cell_edge_current_old, "Cell Edge Current", "Cell Edge", "Flux (1/cm^2)", "Fast Current", "Thermal Current")
 pp.plot_flux(pin_cell_average, "Pin Averaged Flux", "Pin Cell", "Flux (1/cm^2)", "Fast Flux", "Thermal Flux")
-pp.flux_histogram(pin_cell_average, "Pin Averaged Histogram", "Pin Cell", "Flux (1/cm^2)", "Fast Flux", "Thermal Flux")
 
 

post_process.py
@@ -18,6 +18,7 @@ def plot_1d_array(flux, title, x_label, y_label, label_1):
     mpl.ylabel(y_label)
     mpl.show()
 
+
 def pin_cell_average_flux(flux):
     pin_cell = np.reshape(flux,(16,8))
     pin_cell_avg = np.zeros(16)
@@ -26,18 +27,30 @@ def pin_cell_average_flux(flux):
     return pin_cell_avg
 
 
-def flux_histogram(flux, title, x_label, y_label, label_1, label_2):
-    N = len(flux)
-    fast = flux[:, 0]
-    thermal = flux[:, 1]
-    ind = np.arange(0, N)
-    width = 0.5
-    p1 = mpl.bar(ind, thermal, width)
-    p2 = mpl.bar(ind, fast, width, bottom=thermal)
+def flux_histogram(hist_flux, title, x_label, y_label, label_1, label_2, flux):
+    N = len(hist_flux)
+    iter = 0
+    mpl.plot(flux[:,0], label=label_1)
+    mpl.plot(flux[:,1], label=label_2)
+    fast = hist_flux[:, 0]
+    thermal = hist_flux[:, 1]
+    new_thermal = np.zeros(128)
+    new_fast = np.zeros(128)
+    for counter, i in enumerate(new_fast):
+        new_thermal[counter] = thermal[iter]
+        new_fast[counter] = fast[iter]
+        if (counter+1) % 8 == 0:
+                iter += 1
+
+    ind = np.arange(len(new_thermal))
+    width = 1.0
+    p1 = mpl.bar(ind, new_thermal, width, color='c')
+    p2 = mpl.bar(ind, (new_fast-new_thermal), width, color='y',  bottom=new_thermal)
     mpl.title(title)
     mpl.ylabel(y_label)
     mpl.xlabel(x_label)
     mpl.legend((p2[0], p1[0]), (label_1, label_2))
+
     mpl.show()
 
     return